7453b22e273c07e7495d4c7c880d7940acd07c23
Fanny HUJMAN <hujman.fanny@gmail.com>
2025-02-11 11:03:57 +0100

    home page update about searchBar logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TripService } from '../trip/services/trip.service';
+import { Trip } from '../models/trip';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule,CommonModule,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ trip: Trip = {} as Trip; // Objet Trip pour stocker la recherche
+  trips: Trip[] = []; // Liste des trajets trouvés
 
+  @Output() tripsFound = new EventEmitter<Trip[]>(); // Émetteur d'événement pour transmettre les trajets trouvés
+
+  constructor(private service: TripService) {} // Injection du service TripService
+
+  // Méthode pour rechercher les trajets
+  onClick() {
+    this.service.searchTrip(this.trip).subscribe((data: Trip[]) => {
+      this.trips = data; // Stockage des trajets récupérés depuis l'API
+    });
+  }
 }
--- src/app/trip/search-trip/search-trip.component.ts
+++ src/app/trip/search-trip/search-trip.component.ts
@@ -1,24 +1,22 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Trip } from '../../models/trip';
 import { TripService } from '../services/trip.service';
 import { CommonModule } from '@angular/common';
-import { EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-search-trip',
   standalone: true,
-  imports: [CommonModule,FormsModule,RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule], // Supprimer EventEmitter et Output d'ici
   templateUrl: './search-trip.component.html',
-  styleUrl: './search-trip.component.css',
+  styleUrls: ['./search-trip.component.css'],
 })
 export class SearchTripComponent {
   trip: Trip = {} as Trip; // Objet Trip pour stocker la recherche
   trips: Trip[] = []; // Liste des trajets trouvés
-  //selectedTrip: Trip | null = null; // Stocke le trajet sélectionné
 
-  //@Output() tripSelected = new EventEmitter<Trip>(); // Émetteur d'événement pour transmettre le trajet sélectionné
+  @Output() tripsFound = new EventEmitter<Trip[]>(); // Émetteur d'événement pour transmettre les trajets trouvés
 
   constructor(private service: TripService) {} // Injection du service TripService
 
@@ -28,5 +26,5 @@ export class SearchTripComponent {
       this.trips = data; // Stockage des trajets récupérés depuis l'API
     });
   }
-
 }
+
