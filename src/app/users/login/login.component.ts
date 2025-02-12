2716562402a3666a6a7412ec6b1972de8b221b75
Fanny HUJMAN <hujman.fanny@gmail.com>
2025-02-12 10:27:28 +0100

    success alert for login button

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { Login } from '../../models/login';
 import {AuthService} from '../../services/auth.service';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -14,12 +15,19 @@ export class LoginComponent {
   constructor(private auth:AuthService) {}
   login:Login= {} as Login;
   token:string|null =null;
+  successMessage: string | null = null; // Variable pour stocker le message de connexion réussie
 
-  signIn(){
-  
+  signIn() {
     this.auth.login(this.login).subscribe({
-      next: auth => this.auth.setToken(auth.token),
+      next: auth => {
+        this.auth.setToken(auth.token);
+        this.successMessage = 'Connexion réussie !'; // Affichage du message de connexion réussie
+      },
       error: err => console.error('Quelque chose s\'est mal passé :', err)
     });
+  }
+
+  closeAlert() {
+    this.successMessage = null;
   }
 }
